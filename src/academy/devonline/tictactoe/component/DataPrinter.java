3cad5614afea76915aed96328b3cecf0369323c2
NikGit165 <nikitatomilin695@gmail.com>
2022-02-14 19:44:29 +0200

    Implement DataPrinter component

--- src/academy/devonline/tictactoe/component/DataPrinter.java
+++ src/academy/devonline/tictactoe/component/DataPrinter.java
@@ -27,25 +27,26 @@ import academy.devonline.tictactoe.model.GameTable;
  * @link https://github.com/NikGit165
  */
 public class DataPrinter {
+
     public void printMappingTable() {
-        System.out.println("-------------------");
-        System.out.println("|  7  |  8  |  9  |");
-        System.out.println("-------------------");
-        System.out.println("|  4  |  5  |  6  |");
-        System.out.println("-------------------");
-        System.out.println("|  1  |  2  |  3  |");
-        System.out.println("-------------------");
+        System.out.println("-------------");
+        System.out.println("| 7 | 8 | 9 |");
+        System.out.println("-------------");
+        System.out.println("| 4 | 5 | 6 |");
+        System.out.println("-------------");
+        System.out.println("| 1 | 2 | 3 |");
+        System.out.println("-------------");
     }
 
-    public void printGameTable(GameTable gameTable) {
+    public void printGameTable(final GameTable gameTable) {
         for (int i = 0; i < 3; i++) {
-            System.out.println("-------------------");
+            System.out.println("-------------");
             for (int j = 0; j < 3; j++) {
-                System.out.print("|   " + gameTable.getSign(new Cell(i, j)) + " ");
+                System.out.print("| " + gameTable.getSign(new Cell(i, j)) + " ");
             }
             System.out.println("|");
         }
-        System.out.println("-------------------");
+        System.out.println("-------------");
     }
 }
 
