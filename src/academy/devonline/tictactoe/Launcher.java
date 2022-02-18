fe17811c8c5bf90d78b919cc586062498ff3639b
NikGit165 <nikitatomilin695@gmail.com>
2022-02-18 15:12:37 +0200

    Backcup filse

--- src/academy/devonline/tictactoe/Launcher.java
+++ src/academy/devonline/tictactoe/Launcher.java
@@ -28,8 +28,14 @@ import academy.devonline.tictactoe.component.*;
 public final class Launcher {
 
     public static void main(String[] args) {
+        final CellNumberConverter cellNumberConverter = new CellNumberConverter();
+
         final Game game = new Game(
-                new DataPrinter(), new ComputerMove(), new UserMove(), new WinnerVerifier(), new DrawVerifier()
+                new DataPrinter(cellNumberConverter),
+                new ComputerMove(),
+                new UserMove(cellNumberConverter),
+                new WinnerVerifier(),
+                new CellVerifier()
         );
         game.play();
     }
